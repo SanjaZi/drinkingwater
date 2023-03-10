2788c6203927ecb081935358c3f2c64b14655f5d
Sanja Zivkovic <80321654+SanjaZi@users.noreply.github.com>
2023-03-10 10:47:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,42 +2,27 @@ const drinkingglasses = document.querySelectorAll('.drinkingglasses');
 const bigglass = document.getElementById('bigglass');
 
 let counter = 0;
-let reversedcounter = 100;
-
-
+let counterpercent = counter + "%";
 
 drinkingglasses.forEach(element => {
    element.addEventListener('click', function(e) {
-        counter += 12.5;
-        reversedcounter -=12.5;
-        let counterpercent = counter + "%";
-        let reversedcounterpercent = reversedcounter + "%";
-        bigglass.style.background = `linear-gradient(to top, #d4f1f9 ${counterpercent}, #ffffff ${counterpercent})`;
-        // bigglass.style.background = `linear-gradient(180deg, #ffffff ${reversedcounterpercent}, #d4f1f9 ${counterpercent})`;
-        // element.classList.toggle("fullglass");
-        element.style.background = "#d4f1f9"
-        console.log(counterpercent);
+if (counter < 0) {
+    counter = 0;
+}
 
-    
+  if (element.classList.contains("clicked")){
+    counter -=12.5;
+    counterpercent = counter + "%";
+    element.style.background = "white"
+    bigglass.style.background = `linear-gradient(to top, #d4f1f9 ${counterpercent}, #ffffff ${counterpercent})`;
+    element.classList.remove("clicked");
+  } else {
+    counter += 12.5;
+    counterpercent = counter + "%";
+    bigglass.style.background = `linear-gradient(to top, #d4f1f9 ${counterpercent}, #ffffff ${counterpercent})`;
+    element.style.background = "#d4f1f9"
+    element.classList.add("clicked");
+    }
     })
 });
 
-// linear-gradient(to top, red 85%, white)
-
-//version with counter
-// let counter = 0;
-// let counterper = '0%';
-
-
-
-// drinkingglasses.forEach(element => {
-//    element.addEventListener('click', function(e) {
-//         if (counter <= 2000) {
-//         counter +=250;
-//         bigglass.style.background = "linear-gradient(180deg, #ffffff 78%, #d4f1f9 12%)";
-//         element.classList.toggle("fullglass");
-//         // element.style.background = "#d4f1f9"
-//         console.log(counter)
-//     }
-//     })
-// });
